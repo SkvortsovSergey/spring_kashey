283a8416bcb342a0b11770ef4057f120746931e6
Sergey Skvortsov <skvortzovsergey@gmail.com>
2021-11-28 22:26:25 +0300

    final commit

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,5 +1,6 @@
 package koschei.config;
 
+import koschei.models.Deth8;
 import koschei.models.Island2;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,13 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
-    public static Island2 getIsland(Wood3 wood) {
+    public static Island2 getIsland (Wood3 wood) {
+
         return new Island2(wood);
+    }
+
+    @Bean
+    public static Deth8 getDeth () {
+        return new Deth8();
     }
 }
--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -9,12 +9,12 @@ public class Wood3 {
     private final Rabbit4 rabbit;
 
     @Autowired
-    public Wood3(Rabbit4 rabbit) {
+    public Wood3 (Rabbit4 rabbit) {
         this.rabbit = rabbit;
     }
 
     @Override
-    public String toString() {
+    public String toString () {
         return ", на дереве заяц " + rabbit.toString();
     }
 }
